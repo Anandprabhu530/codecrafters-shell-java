54740d620da33dcf29f37084b6cf2fa8b1cbf445
Anandprabhu530 <anandprabhu530@gmail.com>
2025-03-09 20:21:34 +0530

    Type builtin: Executable flie-09

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,38 +1,77 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        HashSet<String> builtInCommands = new HashSet<>();
+        builtInCommands.add("echo");
+        builtInCommands.add("exit");
+        builtInCommands.add("type");
+
         Scanner scanner = new Scanner(System.in);
-        String path = System.getenv("$PATH");
-        String[] availablePath = path.split(":");
-        int index = 1;
         while (true) {
             System.out.print("$ ");
             String input = scanner.nextLine();
-            if (input.startsWith("exit")) {
-                System.exit(0);
-            } else if (input.startsWith("echo")) {
-                System.out.println(input.substring(5));
-            } else if (input.startsWith("type")) {
-                String command = input.substring(5);
-                if (command.equals("echo")) {
-                    System.out.println("echo is a shell builtin");
-                } else if (command.equals("exit")) {
-                    System.out.println("exit is a shell builtin");
-                } else if (command.equals("type")) {
-                    System.out.println("type is a shell builtin");
-                } else {
-                    if (index < availablePath.length) {
-                        System.out.println(
-                                command + " is " + availablePath[index++] + "/" + command);
+            String[] commands = input.split(" ");
+
+            String command = commands[0];
+            String params = "";
+
+            if (commands.length > 2) {
+                for (int i = 1; i < commands.length; i++) {
+                    if (i == commands.length - 1) {
+                        params += commands[i];
                     } else {
-                        System.out.println(command + ": not found");
+                        params += commands[i] + " ";
                     }
-                    System.out.println(path);
                 }
-            } else {
-                System.out.println(input + ": command not found");
+
+            } else if (commands.length > 1) {
+                params += commands[1];
+            }
+
+            switch (command) {
+                case "exit":
+                    if (params.equals("0")) {
+                        System.exit(0);
+                    } else {
+                        System.out.println(input + ": command not found");
+                    }
+                    break;
+
+                case "echo":
+                    System.out.println(params);
+                    break;
+
+                case "type":
+                    if (builtInCommands.contains(params)) {
+                        System.out.println(params + " is a shell builtin");
+                    } else {
+                        String path = getPath(params);
+                        if (path != null) {
+                            System.out.println(params + " is " + path);
+                        } else {
+                            System.out.println(params + ": not found");
+                        }
+                    }
+                    break;
+
+                default:
+                    System.out.println(input + ": command not found");
+                    break;
             }
         }
+    }
+
+    private static String getPath(String pathString) {
+        for (String path : System.getenv("PATH").split(":")) {
+            Path fullPath = Path.of(path, pathString);
+            if (Files.isRegularFile(fullPath)) {
+                return fullPath.toString();
+            }
+        }
+        return null;
     }
 }
